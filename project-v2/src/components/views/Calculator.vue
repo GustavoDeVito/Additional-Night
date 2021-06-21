753a4e1369f1c01fa01dac9885a1e595f9b54de6
Gustavo de Vito <gustavodevito38@gmail.com>
2021-06-21 10:56:14 -0300

    insert form in calculatorScreen

--- project-v2/src/components/views/Calculator.vue
+++ project-v2/src/components/views/Calculator.vue
@@ -1,20 +1,41 @@
 <template>
   <div class="calculator">
-    <div class="mb-3 inputBox">
-      <input
-        type="text"
-        class="form-control"
-        id="formGroupExampleInput"
-        placeholder="Nome"
-        required
-      />
-      <input
-        type="time"
-        class="form-control"
-        id="formGroupExampleInput"
-        placeholder="Adicional Noturno"
-        required
-      />
+    <div class="form">
+      <div class="mb-3 field">
+        <input
+          type="text"
+          class="form-control"
+          id="name"
+          autocomplete="off"
+          v-model="form.name"
+          required
+        />
+        <label for="name" title="Nome"></label>
+      </div>
+      <div class="mb-3 field">
+        <input
+          type="time"
+          class="form-control"
+          id="time"
+          autocomplete="off"
+          v-model="form.time"
+          required
+        />
+        <label for="time" title="Horas"></label>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        time: '00:00',
+      }
+    }
+  }
+}
+</script>
